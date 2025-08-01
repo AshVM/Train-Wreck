745163f4140059f3800b0ea87328ba1a7169e2e1
AshVM <ashlinemichel2008@gmail.com>
2025-08-01 02:02:32 -0400

    added title screen and start button

--- Train Wreck/Assets/Scripts/startToMain.cs
+++ Train Wreck/Assets/Scripts/startToMain.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class wacktestash : MonoBehaviour
+public class startToMain : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // this is a test to check if wackatime/hackatime is working!
-        // other test to make sure wakatime/hackatime is working...
-        // :(
+        
     }
 
     // Update is called once per frame
