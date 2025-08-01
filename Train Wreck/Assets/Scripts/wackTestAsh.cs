394c22620a88f4c3daf2e86926cd937299c41303
AshVM <ashlinemichel2008@gmail.com>
2025-08-01 00:59:03 -0400

    testing

--- Train Wreck/Assets/Scripts/wackTestAsh.cs
+++ Train Wreck/Assets/Scripts/wackTestAsh.cs
@@ -7,7 +7,7 @@ public class wacktestash : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // this is a test to check if wackatime/hackatime is working!
     }
 
     // Update is called once per frame
